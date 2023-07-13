e7994a30f788d65fcaf4f33aa5413cf3e1753612
Raefetoxic <raefetex@gmail.com>
2023-07-13 09:48:57 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - function to print some text
- */
+ * main - the main function is the entry point for the c program
+ *
+ * Description - Simple C program that prints some text to the screen
+ *
+ * Return - Always 0 (Success)
+*/
 
 int main(void)
 {
