cd2bb08beb6bbc704b88673d5f1e3395b82a8b56
Raefetoxic <raefetex@gmail.com>
2023-07-13 11:42:03 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 
 int main(void)
-{	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+{	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int i;
 
 	for (i = 0; text[i] != '\0'; i++)
