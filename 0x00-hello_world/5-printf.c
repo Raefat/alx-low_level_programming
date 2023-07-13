62627f2b039284568f8a8e952c3d273e399664df
Raefetoxic <raefetex@gmail.com>
2023-07-13 10:18:03 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ *
+ * Description: printing some text
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
