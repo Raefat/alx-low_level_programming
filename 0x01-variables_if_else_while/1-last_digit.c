17508eb1c7c382f10cf0b92c70b8d6bccc76a67a
Raefetoxic <raefetex@gmail.com>
2023-07-14 09:51:49 -0700

    task 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program that checks the positivity of last digit
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n;
+	int t;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	t = n > 0 ? n % 10 : -1 * n % 10;
+	if (t < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, t);
+	}
+	else if (t > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5 and not 0\n", n, t);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, t);
+
+	}
+	return (0);
+}
+
