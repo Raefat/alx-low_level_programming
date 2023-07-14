224786b24dfbbbfd364f516ec14c38fddbe6cecb
Raefetoxic <raefetex@gmail.com>
2023-07-14 14:53:04 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,30 +12,20 @@ int main(void)
 {
 	int n;
 	int j;
-	int k;
-	int m;
 
-	for (n = 0; n < 9; n++)
+	for (n = 0; n < 99; n++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = n + 1; j <= 99; j++)
 		{
-			for (k = 0; k <= 9; k++)
-			{
-				for (m = 0; m <= 9; m++)
-				{
-					if (n == 0 && j == 0 && k == 0 && m == 0)
-						continue;
-					putchar('0' + n);
-					putchar('0' + j + n + 1);
-					putchar(' ');
-					putchar('0' + k + n + j + 1);
-					putchar('0' + m + k + j + n + 1);
-					if (n == 9 && j == 8 && k == 9 && m == 9)
-						break;
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar(n / 10 + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar((j % 10) + '0');
+			if (n == 99 && j == 98)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
