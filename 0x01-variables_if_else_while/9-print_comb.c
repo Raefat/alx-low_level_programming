c32c2edb04a62fb48018fd7fa84debee70fd13d8
Raefetoxic <raefetex@gmail.com>
2023-07-14 11:23:05 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program that prints all comb. of single-digit nums
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar('0' + c);
+		putchar(', ');
+	}
+	putchar('\n');
+	return (0);
+}
+
