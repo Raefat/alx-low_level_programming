a31cb7ce37422aa1008d0bc33b4984fe350df04f
Raefetoxic <raefetex@gmail.com>
2023-07-14 10:59:21 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program that prints alphabets in reverse order
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
