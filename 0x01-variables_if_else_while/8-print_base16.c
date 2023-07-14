fb1fbe4fb57be5a098b5f22bd1d4becb13ff8764
Raefetoxic <raefetex@gmail.com>
2023-07-14 11:15:59 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program that prints numbers of base16
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 16; c++)
+	{
+		if (c < 10)
+		{
+			putchar('0' + c);
+		}
+		if (c >= 10)
+		{
+			putchar('a' - (c - 10));
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
