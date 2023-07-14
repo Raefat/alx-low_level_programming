6a77daf0a801381b32e25886ea76d46b6b918a82
Raefetoxic <raefetex@gmail.com>
2023-07-14 12:25:56 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program that prints numbers combinations
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 9; n++)
+	{
+		for (int j = n; j <= 9; j++)
+		{
+			putchar('0' + n);
+			putchar('0' + j);
+			if (n == 8 && j == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
