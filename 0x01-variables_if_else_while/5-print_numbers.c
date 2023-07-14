6e87792c0c953294bdff743d1fa2debb20740ad2
Raefetoxic <raefetex@gmail.com>
2023-07-14 10:42:43 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program that prints numbers from 0 to 10
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf('\n');
+	return (0);
+}
+
