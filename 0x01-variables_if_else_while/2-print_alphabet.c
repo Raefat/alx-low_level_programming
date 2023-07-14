f5d6a96e8a02b34793e1e0b9538ef71808f891e5
Raefetoxic <raefetex@gmail.com>
2023-07-14 10:07:38 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: program that prints alphabets that are lower case
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char c = 97;
+
+	while (c < (97 + 26))
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
+
