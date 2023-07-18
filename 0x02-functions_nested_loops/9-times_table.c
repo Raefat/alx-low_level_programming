95b975173847694bf80ffacd8a0e0e8f4b0af72e
Raefetoxic <raefetex@gmail.com>
2023-07-18 08:39:55 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,24 +14,28 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (i * j < 10)
+			if (i <= 9 && j == 0)
 			{
-				_putchar('0' + j * i);
+				_putchar('0');
+				_putchar(',');
+			}
+			else if (i * j < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + i * j);
 				if (j == 9)
 					break;
 				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
 			}
 			else
 			{
+				_putchar(' ');
 				_putchar('0' + (i * j) / 10);
 				_putchar('0' + (i * j) % 10);
 				if (j == 9)
 					break;
-				_putchar(',');
-				_putchar(' ');
-
+				putchar(',');
 			}
 		}
 		_putchar('\n');
