ca3b682233cb5a0d2d63af400dcc221528c0b34f
Raefetoxic <raefetex@gmail.com>
2023-07-18 09:09:41 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,14 +14,20 @@ void print_to_98(int n)
 	while (n >= 98)
 	{
 		if (n == 98)
+		{
+			printf("%d", n);
 			break;
+		}
 		printf("%d, ", n);
 		n--;
 	}
 	while (n <= 98)
 	{
 		if (n == 98)
+		{
+			printf("%d", n);
 			break;
+		}
 		printf("%d, ", n);
 		n++;
 	}
