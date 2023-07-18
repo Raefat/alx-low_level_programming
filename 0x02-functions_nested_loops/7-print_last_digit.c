87577e8950542f24abd10542e3b2418b2e9526ee
Raefetoxic <raefetex@gmail.com>
2023-07-18 06:03:12 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that returns last digit of a number
+ *
+ * @n: the number that we will return its last digit
+ *
+ * Return: last digit of @n
+*/
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
