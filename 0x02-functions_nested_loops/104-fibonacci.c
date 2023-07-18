8c39443a70ea049faab16bac185abe9856128117
Raefetoxic <raefetex@gmail.com>
2023-07-18 12:42:41 -0700

    task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,22 +10,31 @@
 
 int main(void)
 {
-	unsigned long int first = 0, second = 1;
-	int i = 0;
-	unsigned long int fibo;
+	unsigned long int fraction1 = 0, first = 1, fraction2 = 0, second = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
 
-	while (i < 98)
+	printf("%lu, %lu, ", first, second);
+	for (count = 2; count < 98; count++)
 	{
-		fibo = first + second;
-		if (i == 97)
+		if (first + second > 10000000000 || fraction2 > 0 || fraction1 > 0)
 		{
-			printf("%lu\n", fibo);
-			break;
+			hold1 = (first + second) / 10000000000;
+			hold2 = (first + second) % 10000000000;
+			hold3 = fraction1 + fraction2 + hold1;
+			fraction1 = fraction2, fraction2 = hold3;
+			first = second, second = hold2;
+			printf("%lu%010lu", fraction2, second);
 		}
-		printf("%lu, ", fibo);
-		first = second;
-		second = fibo;
-		i++;
+		else
+		{
+			hold2 = first + second;
+			first = second, second = hold2;
+			printf("%lu", second);
+		}
+		if (count != 97)
+			printf(", ");
 	}
+	printf("\n");
 	return (0);
 }
