28dbd36ac5b9b8d0477b66ece94f9e0c8511fa77
Raefetoxic <raefetex@gmail.com>
2023-07-18 05:29:31 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,13 @@
 #include "main.h"
 
 /**
- * _islower - function that prints alphabets 10 times
+ * _islower - function that checks if character is lowercase
  *
  * Return: 0 or 1
 */
 
 int _islower(int c)
 {
-	int isLower;
-
 	if (c >= 97 && c <= 122)
 	{
 		return (1);
