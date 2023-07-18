49e42a684cb0b3bda5c2bd6ea55c4d8e53d4f0ab
Raefetoxic <raefetex@gmail.com>
2023-07-18 10:24:44 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,50 +12,35 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	if (n <= 0 || n > 15)
+	if (n < 0 || n > 15)
 	{
-		return;
-	}
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			if (j == 0)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
-				_putchar('0');
 				_putchar(',');
+				_putchar(' ');
+				if (i * j >= 100)
+				{
+					_putchar('0' + (i * j) / 100);
+					_putchar('0' + ((i * j) / 10) % 10);
+					_putchar('0' + (i * j) % 10);
+				}
+				else if (i * j < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + i * j);
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar('0' + (i * j) / 10);
+					_putchar('0' + (i * j) % 10);
+				}
 			}
-			else if (i * j < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + i * j);
-				if (j == n)
-					break;
-				_putchar(',');
-			}
-			else if (i * j < 100)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + (i * j) / 10);
-				_putchar('0' + (i * j) % 10);
-				if (j == n)
-					break;
-				_putchar(',');
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar('0' + (i * j) / 100);
-				_putchar('0' + ((i * j) / 10) % 10);
-				_putchar('0' + (i * j) % 10);
-				if (j == n)
-					break;
-				_putchar(',');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
