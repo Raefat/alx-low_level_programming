67ea4df3868747bf1c52e970384b4bfaee7ea6a2
Raefetoxic <raefetex@gmail.com>
2023-07-18 08:48:39 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that adds two numbers
+ *
+ * Return: sum of two numbers
+*/
+
+int add(int i, int j)
+{
+	int i, j;
+
+	return (i + j);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints 9 times table
+ *
+ * Return: void
+*/
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i <= 9 && j == 0)
+			{
+				_putchar('0');
+				_putchar(',');
+			}
+			else if (i * j < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + i * j);
+				if (j == 9)
+					break;
+				_putchar(',');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + (i * j) / 10);
+				_putchar('0' + (i * j) % 10);
+				if (j == 9)
+					break;
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
