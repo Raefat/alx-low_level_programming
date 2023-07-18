b8bc77f984bfb39e1e6a5c4bc970036bfe4e5294
Raefetoxic <raefetex@gmail.com>
2023-07-18 05:46:11 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - function that checks if character is alphabetic
+ *
+ * @n: the character that needs to be checked
+ *
+ * Return: 0 or 1 or -1
+*/
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	return (0);
+}
