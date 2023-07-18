bc9f1c939dc37b980274b0c62b35b5243bb6129f
Raefetoxic <raefetex@gmail.com>
2023-07-18 11:47:18 -0700

    task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main  - entry point
+ *
+ * Description: program that prints fibonacci numbers
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	int first = 0, second = 1;
+	int i = 0;
+	int fibo;
+
+	while (i < 50)
+	{
+		fibo = first + second;
+		printf("%d, ", fibo);
+		first = second;
+		second = fibo;
+		i++;
+	}
+	return (0);
+}
