7e101c32ea4c30fd5a3218d906be8f5b55a21c13
Raefetoxic <raefetex@gmail.com>
2023-07-18 05:57:02 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - function that returns absolute value of a number
+ *
+ * @n: the character that we will return its absolute value
+ *
+ * Return: absolute value of @n
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-1 * n);
+	}
+	else if (n >= 0)
+	{
+		return (n);
+	}
+}
