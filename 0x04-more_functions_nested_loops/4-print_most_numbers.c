84db4549163fb2cbb1bfbcf1bb07de836c010af6
Raefetoxic <raefetex@gmail.com>
2023-07-20 10:26:11 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints from 0 to 9
+ * except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
