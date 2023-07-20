71e7fd13d5f001af31926bf6939fc2f9f2db6522
Raefetoxic <raefetex@gmail.com>
2023-07-20 11:18:15 -0700

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,15 +12,16 @@ void print_diagonals(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		for (j = 0; j < i; j++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\\');
-		_putchar('\n');
-	}
-	if (n == 0)
-		_putchar('\n');
+	}	
 }
