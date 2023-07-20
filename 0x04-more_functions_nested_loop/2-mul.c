a1ca8c91e58e02365daeed83acda111f6af29c7d
Raefetoxic <raefetex@gmail.com>
2023-07-20 10:12:25 -0700

    task 2

---
+++ 0x04-more_functions_nested_loop/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies 2 numbers
+ *
+ * @a: the first number to multiply
+ * @b: the second number
+ *
+ * Return: 0 if is not uppercase and 1 otherwise
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
