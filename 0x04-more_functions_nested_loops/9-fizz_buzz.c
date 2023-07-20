698243bcb0ee3b1637c09b13c189d68003a4666e
Raefetoxic <raefetex@gmail.com>
2023-07-20 11:53:42 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,8 +19,8 @@ int main(void)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
-		else if (i % 5)
-			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
 		else
 			printf("%d ", i);
 	}
