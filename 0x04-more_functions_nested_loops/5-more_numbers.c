9288f61b0323f66b3c8435088a4c4bf09b4108a0
Raefetoxic <raefetex@gmail.com>
2023-07-20 10:50:11 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,9 +15,12 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar('0' + j);
+			if (j >= 10)
+			{
+				_putchar('0' + j / 10);
+			}
+			_putchar('0' + j % 10);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
