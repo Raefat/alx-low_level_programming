5e9b817080093e90ebe8b87b4955043144a0a0f2
Raefetoxic <raefetex@gmail.com>
2023-07-21 11:40:05 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,8 @@
-#include "main.h"
 #include <math.h>
+#include "main.h"
+
 /**
  * print_number - function that prints an integer
- *
  * @n: The number that will be printed
  *
  * Return: void
@@ -12,6 +12,12 @@ void print_number(int n)
 {
 	int num = n, numOfDivision = 0;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+		return;
+	}
 	if (n < 0)
 	{
 		n = -n;
