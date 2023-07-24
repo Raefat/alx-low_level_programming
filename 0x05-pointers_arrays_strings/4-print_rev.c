60a0b085700bb029df0994402a46dadeabf20f9d
Raefetoxic <raefetex@gmail.com>
2023-07-24 12:25:45 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - function that prints a string, in reverse
+ * followed by a new line.
+ *
+ * @s: pointer to a string
+ */
+
+void print_rev(char *s)
+{
+	char firstLetter = s;
+
+	while (*s != '\0')
+	{
+		s = s - 1;
+	}
+	while (*s != firstLetter)
+	{
+		s--;
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
