c30a6092ed0648c12ec2bc22527381c2e42763ea
Raefetoxic <raefetex@gmail.com>
2023-07-24 14:55:51 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - function that prints half of a string
+ * followed by a new line.
+ *
+ * @str: pointer to a string
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, end = 0;
+
+	while (str[end] > '\0')
+		end++;
+	for (i = 0; i < end; i++)
+	{
+		if (i >= (end / 2))
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
