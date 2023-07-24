b2d7851a87842c7def35ce098ae5aa8cbb1d13ed
Raefetoxic <raefetex@gmail.com>
2023-07-24 15:27:23 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,7 +10,7 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		if (i == n - 1)
@@ -20,5 +20,5 @@ void print_array(int *a, int n)
 		}
 		printf("%d, ", a[i]);
 	}
-	printf("\n"); 
+	printf("\n");
 }
