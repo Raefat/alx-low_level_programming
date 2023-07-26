5cb6606520552a4677b4fa5f99ff21c4b3e87a07
Raefetoxic <raefetex@gmail.com>
2023-07-26 12:31:03 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - function that changes all lowercase letters
+ * of a string to uppercase.
+ *
+ * @s: pointer to string
+ *
+ * Return: uppercase string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 1, d = 0;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= 'a' && s[i] <= 'z'))
+		{
+			if (s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t'
+				|| s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '\"'
+				|| s[i - 1] == '.' || s[i - 1] == '(' || s[i - 1] == ')'
+				|| s[i - 1] == '{' || s[i - 1] == '}' || s[i - 1] == '?'
+				|| s[i - 1] == '!')
+			{
+				s[i] -= 32;
+			}
+		}
+	}
+	return (s);
+}
