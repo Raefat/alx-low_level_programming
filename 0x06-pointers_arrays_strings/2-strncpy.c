adf232ef962ba57f7135e184ad2c4be1919bf947
Raefetoxic <raefetex@gmail.com>
2023-07-26 10:19:18 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that coppies two strings.
+ *
+ * @dest: pointer to first string
+ * @src: pointer to the second string
+ * @n: number of letters to coppy from @src
+ * Return: pointer the coppied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
