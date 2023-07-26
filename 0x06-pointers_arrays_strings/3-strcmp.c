a2567fe8b02412aaf4f1fbade6e22025b3510abf
Raefetoxic <raefetex@gmail.com>
2023-07-26 10:49:27 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,14 +8,14 @@
  * Return: -1 or 0 if string are identical
  */
 
-int *_strncpy(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
 	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
-			return -1
+			return (-1);
 	}
 
 	return (0);
