3f89bc751095f4ac33c3ec37afacded7455c1f8f
Raefetoxic <raefetex@gmail.com>
2023-07-27 00:30:52 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer.
+ *
+ * @n: integer
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n < 10)
+		_putchar('0' + n);
+	else
+	{
+		print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+}
