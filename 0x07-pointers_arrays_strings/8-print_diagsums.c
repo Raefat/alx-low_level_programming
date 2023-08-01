fd02dbae62023d32946a5b71590b70eef7cc65a9
Raefetoxic <raefetex@gmail.com>
2023-07-31 17:03:11 -0700

    task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,6 +13,11 @@ void print_diagsums(int *a, int size)
 {
 	int i, j, sum1 = 0, sum2 = 0;
 
+	if (size == 1)
+	{
+		printf("%d\n", a[0]);
+		return;
+	}
 	for (i = 0; i < size * size; i += size + 1)
 		sum1 += a[i];
 	for (j = size * size - size; j >= 0; j -= size - 1)
