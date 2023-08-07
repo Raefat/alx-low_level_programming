48215dfce95cb8b7982cb34e95b0edeae91b2ef4
Raefetoxic <raefetex@gmail.com>
2023-08-07 13:30:38 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *p;
-	int i, len1 = 0, len2 = 0;
+	int i, j, len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
 		s1 =  "";
@@ -32,8 +32,8 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < len1; i++)
 		p[i] = s1[i];
-	for (; i < len2; i++)
-		p[i] = s2[i];
+	for (j = 0; j < len2; j++)
+		p[i + j] = s2[j];
 	p[len1 + len2 + 1] = '\0';
 	return (p);
 }
