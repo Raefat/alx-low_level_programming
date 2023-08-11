268dd7265ee344dc5ffd8388193774bae641a7b5
Raefetoxic <raefetex@gmail.com>
2023-08-10 17:42:37 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,4 +10,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(size * nmemb);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < (size * nmemb); i++)
+		p[i] = '\0';
+	return (p);
 }
