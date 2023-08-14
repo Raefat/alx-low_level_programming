69fb5d97267368a7a21251eb4b305f2063405755
Raefetoxic <raefetex@gmail.com>
2023-08-14 09:09:00 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,21 @@
 #ifndef _DOG_H
 #define _DOG_H
 
+/**
+ * struct dog - dog structor
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
-	char *owner; 
+	char *owner;
 };
 
 #endif
