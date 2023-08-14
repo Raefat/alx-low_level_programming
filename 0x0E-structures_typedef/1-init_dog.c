778f94861f8056173e4f59d956250add6c14b568
Raefetoxic <raefetex@gmail.com>
2023-08-14 09:17:39 -0700

    task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,12 +7,15 @@
  * @dog: dog instance
  * @name: name of the dog
  * @age: age of dog
- * @owner: owner name 
+ * @owner: owner name
  * Return: void
- */ 
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if(d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
