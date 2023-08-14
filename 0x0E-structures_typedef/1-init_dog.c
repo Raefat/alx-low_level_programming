913127195feb4f6aabf7cda3ba4cda29728eeba6
Raefetoxic <raefetex@gmail.com>
2023-08-14 09:16:07 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+
+/**
+ * init_dog - functiona that initialiaze a dog struct
+ * @dog: dog instance
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: owner name 
+ * Return: void
+ */ 
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
