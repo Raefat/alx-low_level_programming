42813dfd1c9a1741af6294af2fbe0de50cd39557
Raefetoxic <raefetex@gmail.com>
2023-08-14 09:35:56 -0700

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,16 +11,16 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (d->name)
-			printf("name: %s\n", d->name);
+			printf("Name: %s\n", d->name);
 		else
-			printf("name: (nil)\n");
+			printf("Name: (nil)\n");
 		if (d->age)
-			printf("agee: %f\n", d->age);
+			printf("Age: %f\n", d->age);
 		else
-			printf("age: (nil)\n");
+			printf("Age: (nil)\n");
 		if (d->owner)
-			printf("owner: %s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 		else
-			printf("owner: (nil)\n");
+			printf("Owner: (nil)\n");
 	}
 }
