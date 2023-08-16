f917b134e2dc79fb9deb1bc359698bb60409e50f
Raefetoxic <raefetex@gmail.com>
2023-08-16 14:11:50 -0700

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: arg values
+ * Description: program that
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*op)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op = get_op_fun(argv[2]);
+
+	if (op ==  NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
