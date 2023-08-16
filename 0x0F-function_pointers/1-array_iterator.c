e849237780946cec5010700da5c6b3c2fd4b7c23
Raefetoxic <raefetex@gmail.com>
2023-08-16 12:15:46 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that executes a function given 
+ * as a parameter on each element of an array.
+ * @array: array to be procecced 
+ * @size: siz eof the array
+ * @action: poiner to a fucntion
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
