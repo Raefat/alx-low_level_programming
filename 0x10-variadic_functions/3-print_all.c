947eda2318abcd922165bdfc4c102f76a01f43e2
Raefetoxic <raefetex@gmail.com>
2023-08-17 09:45:56 -0700

    tesk done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,6 +14,7 @@ void print_all(const char * const format, ...)
 	unsigned int i = 0, len = 0;
 	va_list args;
 	char *str;
+	int marker = 0;
 
 	while (format[len])
 		len++;
@@ -24,23 +25,28 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%c", va_arg(args, int));
+				marker = 0;
 				break;
 			case 'i':
 				printf("%d", va_arg(args, int));
+				marker = 0;
 				break;
 			case 'f':
 				printf("%f", va_arg(args, double));
+				marker = 0;
 				break;
 			case 's':
 				str = va_arg(args, char*);
+				marker = 0;
 				if (str == NULL)
 					str = "(nil)";
 				printf("%s", str);
 				break;
 			default:
+				marker = 1;
 				break;
 		}
-		if (i < len - 1)
+		if (i < len - 1 && marker != 1)
 			printf(", ");
 		i++;
 	}
