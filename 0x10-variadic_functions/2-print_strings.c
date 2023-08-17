e15cbd7b3f024de57fc52e25723e3312f20aef81
Raefetoxic <raefetex@gmail.com>
2023-08-17 10:14:00 -0700

    tesk done

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 
 	if (n == 0)
+	{
+		print("\n");
 		return;
+	}
 	va_start(args, n);
 	separator = separator == NULL ? "" : separator;
 	for (i = 0; i < n - 1; i++)
