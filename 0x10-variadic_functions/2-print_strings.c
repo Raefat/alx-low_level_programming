a0b0bf2ec4ae3efddde03ee4547302ad718ba008
Raefetoxic <raefetex@gmail.com>
2023-08-17 07:31:57 -0700

    task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,14 +3,13 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - function that prints numbers, followed by a new line.
+ * print_strings - function that prints strings, followed by a new line.
  *
  * @separator: pointer to a string of separators
  * @n: number of variadic params
  * Return: void
  */
-
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list args;
@@ -18,7 +17,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	separator = separator == NULL ? "" : separator;
 	for (i = 0; i < n - 1; i++)
-		printf("%d%s", va_arg(args, int), separator);
-	printf("%d\n", va_arg(args, int));
+	{
+		if (va_arg(args, char*) == NULL)
+			printf("nil");
+		else
+			printf("%s%s", va_arg(args, char*), separator);
+	}
+	printf("%s\n", va_arg(args, char*));
 	va_end(args);
 }
