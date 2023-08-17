9bf1143bf6a994ce2ae32e87b04a8ceb05a59eba
Raefetoxic <raefetex@gmail.com>
2023-08-17 07:11:03 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,13 +16,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
+	separator = separator == NULL ? "" : separator;
 	for (i = 0; i < n - 1; i++)
-	{
-		if (separator != NULL)
-			printf("%d%s", va_arg(args, int), separator);
-		else
-			printf("%d", va_arg(args, int));
-	}
+		printf("%d%s", va_arg(args, int), separator);
 	printf("%d\n", va_arg(args, int));
 	va_end(args);
 }
