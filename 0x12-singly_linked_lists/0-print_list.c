c01158087453209260e71b3981b5d9e1f23d3b58
Raefetoxic <raefetex@gmail.com>
2023-08-24 11:45:36 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,9 +15,9 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[%d] (nil)", h->len);
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
