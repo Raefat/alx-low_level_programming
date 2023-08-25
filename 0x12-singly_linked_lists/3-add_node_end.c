d8befc2651d96f1781ed1e323895c94864d56292
Raefetoxic <raefetex@gmail.com>
2023-08-24 17:36:47 -0700

    task 2

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * add_node - function that adds a new node at
- * the beginning of a list_t list
- *
+ * add_node_end - function that adds a new node at
+ * the end of a list_t list
  * @head: pointer to a headNode
  * @str: pointer to the string
  * Return: new list node
  */
-list_t *add_node(list_t **head, const char *str)
+
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newNode;
 
@@ -18,7 +18,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	newNode->str = strdup(str);
 	newNode->len = strlen(str);
-
+	
 	if (*head == NULL)
 		newNode->next = NULL;
 	else
