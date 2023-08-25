1dcce95c2b081a4852208db547c75be00e1077bd
Raefetoxic <raefetex@gmail.com>
2023-08-25 03:10:50 -0700

    task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 /**
- * before_main - contructor to execute before_main it before main
+ * before_main_fun - constructor to execute before_main() it before main
  *
  */
-
-void before_main(void) __attribute__ ((constructor));
+void before_main_fun(void) __attribute__ ((constructor));
 
 /**
- * before_main - implementation of before_main
+ * before_main_fun - implementation of before_main
  *
  */
-
-void before_main(void)
+void before_main_fun(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
