d6b3c2709e4c90680e5d5957b75e8fb24d1f7fa6
Raefetoxic <raefetex@gmail.com>
2023-08-24 18:15:43 -0700

    task 3

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list from memory
+ * @head: head of the linked list.
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *curr;
+	
+	curr = head;
+	while (curr != NULL)
+	{
+		curr = head->next;
+		free(curr->str);
+		free(curr);
+	}
+}
