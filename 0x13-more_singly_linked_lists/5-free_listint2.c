26a34c1c4b323859ebf44b08aa9939c083d1921a
Raefetoxic <raefetex@gmail.com>
2023-08-29 16:15:12 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees istint_t list
+ * @head: head node
+ * Return: void
+*/
+
+void free_listint2(listint_t *head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
