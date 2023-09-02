e764bd264fda5f17c4af233f3f0b6c635f54217c
Raefet Ouafiq <43799678+Raefat@users.noreply.github.com>
2023-09-02 01:03:40 +0100

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * clear_bit - function that clears a bit at a given index.
+ * clear_bit - function that clears a bit at a given index
  * @n: pointer to a given number
  * @index: given index
  * Return: 1 if it worked, or -1 if an error occurred
