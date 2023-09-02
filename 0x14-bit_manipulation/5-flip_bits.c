340f8b89b61239854c4bb0a84c431f047337ab39
Raefetoxic <raefetex@gmail.com>
2023-09-01 17:05:33 -0700

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,10 +12,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int xor = n ^ m, bits = 0;
 
-	while (xor > 1)
+	while (xor > 0)
 	{
 		bits += (xor & 1);
-		xor >> 1;
+		xor = xor >> 1;
 	}
 	return (1);
 }
