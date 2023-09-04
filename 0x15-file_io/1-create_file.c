ee40f9bda6b1748bf6b14189f6b92700f63abbda
Raefetoxic <raefetex@gmail.com>
2023-09-04 15:15:38 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: pointer to file name
+ * @text_content: text to add to file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	while (text_content[len])
+		len++;
+	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (!text_content)
+		text_content = "";
+	w = write(STDOUT_FILENO, text_content, len);
+	if (w == -1 || o == -1)
+		return (-1);
+	close(o);
+	return (1);
+}
