09b86879f68ffb7f2947dc6c9ca9de97797fa6d3
Raefetoxic <raefetex@gmail.com>
2023-09-04 15:26:10 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	o = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (!text_content)
 		text_content = "";
-	w = write(STDOUT_FILENO, text_content, len);
+	w = write(o, text_content, len);
 	if (w == -1 || o == -1)
 		return (-1);
 	close(o);
