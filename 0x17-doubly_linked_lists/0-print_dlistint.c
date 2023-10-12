ac6f8c91fb39b35c7e14734e6f81bb68467823d6
Raefat <raefetex@gmail.com>
2023-10-12 08:24:50 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,7 @@
+#include "lists.h"
 
-/** print_dlistint - function that prints all the elements of a dlistint_t list
+/**
+ * print_dlistint - function that prints all the elements of a dlistint_t list
  * @h: pointer to the dll
  * Return: the number of nodes
  */
@@ -7,7 +9,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
-	
+
 	if (h == NULL)
 		return (0);
 	while (h)
